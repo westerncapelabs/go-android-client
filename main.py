45d474deaecf077e3895ecd6c5639d943ef1f6ef
Ngoni Munyaradzi <ngoni.munyaradzi@gmail.com>
2014-09-17 23:16:58 +0200

    convert EndStateWidget to screen & display via screenmanager

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class EndStateWidget(BoxLayout):
+class EndState(Screen):
+	pass
+
+class MyScreenManager(ScreenManager):
 	pass
 
 class AndroidClient(App):
     def build(self):
-		return EndStateWidget()
+	self.endstate = EndState()
+	root = MyScreenManager()
+	root.add_widget(self.endstate)
+	return root
 
 if __name__ == "__main__":
 	AndroidClient().run()
