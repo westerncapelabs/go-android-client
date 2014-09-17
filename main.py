d6ca78351c259c33afc0b865eb79f0c4d69ee4dc
Ngoni Munyaradzi <ngoni.munyaradzi@gmail.com>
2014-09-17 23:40:57 +0200

    custom textinput widget

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from kivy.app import App
 
+from kivy.properties import NumericProperty
+
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.textinput import TextInput
 
 class FreeTextState(Screen):
 	pass
@@ -10,6 +13,14 @@ class EndState(Screen):
 
 class MyScreenManager(ScreenManager):
 	pass
+
+class CustomTextInput(TextInput):
+	max_chars = NumericProperty(160)
+
+	def insert_text(self, substring, from_undo=False):
+		if not from_undo and (len(self.text)+len(substring) > self.max_chars):
+			return
+		super(CustomTextInput, self).insert_text(substring, from_undo)
 
 class AndroidClient(App):
     def build(self):
